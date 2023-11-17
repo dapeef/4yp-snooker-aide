20c514973f3dfcba5b9644bd716ed2bfc0bb204d
dapeef <42572235+dapeef@users.noreply.github.com>
2023-11-17 12:33:25 +0000

    Tidy up evaluator

--- pytorch_detection_eval.py
+++ pytorch_detection_eval.py
@@ -7,7 +7,6 @@ import numpy as np
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
 import os
-import utils
 
 # Function to visualize bounding boxes in the image
 def plot_img_bbox(img, target):
@@ -20,7 +19,7 @@ def plot_img_bbox(img, target):
         high_score = True
 
         if "scores" in target.keys():
-            if target['scores'][i] < 0.7:
+            if target['scores'][i] < 0.2:
                 high_score = False
 
         if high_score and target["labels"][i]==1:
@@ -54,16 +53,12 @@ class TableImagesDataset(torch.utils.data.Dataset):
     def __init__(self, files_dir, width, height, transforms=None):
         self.transforms = transforms
         self.images_dir = files_dir + "\\images"
-        self.labels_dir = files_dir + "\\labels"
         self.height = height
         self.width = width
         
         # sorting the images for consistency
         # To get images, the extension of the filename is checked to be jpg
         self.imgs = [image for image in sorted(os.listdir(self.images_dir)) if image[-4:]=='.jpg']
-        
-        # classes: 0 index is reserved for background
-        self.classes = ['table', 'pocket', 'cushion']
 
     def __getitem__(self, idx):
         img_name = self.imgs[idx]
@@ -75,69 +70,21 @@ class TableImagesDataset(torch.utils.data.Dataset):
         img_res = cv2.resize(img_rgb, (self.width, self.height), cv2.INTER_AREA)
         # diving by 255
         img_res /= 255.0
-        
-        # annotation file
-        annot_filename = img_name[:-4] + '.txt'
-        annot_file_path = os.path.join(self.labels_dir, annot_filename)
-        
-        boxes = []
-        labels = []
-        
-        # cv2 image gives size as height x width
-        wt = img.shape[1]
-        ht = img.shape[0]
-        
-        # box coordinates for xml files are extracted and corrected for image size given
-        with open(annot_file_path) as f:
-            for line in f:
-                parsed = [float(x) for x in line.split(' ')]
-                labels.append(parsed[0])
-                x_center = parsed[1]
-                y_center = parsed[2]
-                box_wt = parsed[3]
-                box_ht = parsed[4]
-
-                xmin = x_center - box_wt/2
-                xmax = x_center + box_wt/2
-                ymin = y_center - box_ht/2
-                ymax = y_center + box_ht/2
-                
-                xmin_corr = int(xmin*self.width)
-                xmax_corr = int(xmax*self.width)
-                ymin_corr = int(ymin*self.height)
-                ymax_corr = int(ymax*self.height)
-                
-                if xmax_corr - xmin_corr == 0 or ymax_corr - ymin_corr == 0:
-                    print("YIKES")
-                
-                boxes.append([xmin_corr, ymin_corr, xmax_corr, ymax_corr])
-        
-        # convert boxes into a torch.Tensor
-        boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        
-        # getting the areas of the boxes
-        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-
-        # suppose all instances are not crowd
-        iscrowd = torch.zeros((boxes.shape[0],), dtype=torch.int64)
-        
-        labels = torch.as_tensor(labels, dtype=torch.int64)
 
         target = {}
-        target["boxes"] = boxes
-        target["labels"] = labels
-        target["area"] = area
-        target["iscrowd"] = iscrowd
-        image_id = torch.tensor([idx])
-        target["image_id"] = image_id
+        target["boxes"] = torch.as_tensor([], dtype=torch.float32)
+        target["labels"] = torch.as_tensor([], dtype=torch.int64)
+        target["image_id"] = torch.tensor([idx])
 
         if self.transforms:
             sample = self.transforms(image = img_res,
                                     bboxes = target['boxes'],
-                                    labels = labels)
+                                    labels = target["labels"])
             img_res = sample['image']
             target['boxes'] = torch.Tensor(sample['bboxes'])
-            
+        
+        print(target)
+
         return img_res, target
 
     def __len__(self):
@@ -168,18 +115,19 @@ checkpoint = torch.load(".\\checkpoints\\model.pth")
 model.load_state_dict(checkpoint['model_state_dict'])
 model.eval()
 
-dataset = TableImagesDataset('data\\Pockets, cushions, table - 2688 - B&W, rotated, mostly 9 ball\\test_small', 244, 244, transforms=get_transform(False))
+dataset = TableImagesDataset('data\\Pockets, cushions, table - 2688 - B&W, rotated, mostly 9 ball\\real_test', 244, 244, transforms=get_transform(False))
 
 with torch.no_grad():
-    item = dataset[0]
-    image = item[0]
-    targets = item[1]
+    for i in range(15):
+        item = dataset[i]
+        image = item[0]
+        targets = item[1]
 
-    # plot_img_bbox(image.permute(1, 2, 0), targets)
+        # plot_img_bbox(image.permute(1, 2, 0), targets)
 
-    images = list(img.to("cpu") for img in [image])
-    pred = model(images)
+        images = list(img.to("cpu") for img in [image])
+        pred = model(images)
 
-plot_img_bbox(image.permute(1, 2, 0), pred[0])
+        plot_img_bbox(image.permute(1, 2, 0), pred[0])
 
 print(pred)
