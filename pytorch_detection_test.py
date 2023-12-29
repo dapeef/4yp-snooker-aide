ca7949228c0d808626cccee6db5a827971b6f262
dapeef <42572235+dapeef@users.noreply.github.com>
2023-12-29 13:25:40 +0000

    Revert to using albumentations

--- pytorch_detection_test.py
+++ pytorch_detection_test.py
@@ -18,7 +18,7 @@ import matplotlib.patches as patches
 # torchvision libraries
 import torch
 import torchvision
-from torchvision.transforms import v2
+# from torchvision.transforms import v2
 # from torchvision.transforms import ToTensor
 # from torchvision import transforms as torchtrans  
 # from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
@@ -29,13 +29,14 @@ import utils
 import transforms as T
 
 # for image augmentations
-# import albumentations as A
-# from albumentations.pytorch.transforms import ToTensorV2 # Depricated
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
 
 
 
 # we create a Dataset class which has a __getitem__ function and a __len__ function
 class TableImagesDataset(torch.utils.data.Dataset):
+
     def __init__(self, files_dir, width, height, transforms=None):
         self.transforms = transforms
         self.images_dir = files_dir + "/images"
@@ -117,10 +118,9 @@ class TableImagesDataset(torch.utils.data.Dataset):
         target["image_id"] = image_id
 
         if self.transforms:
-            sample = self.transforms({"image": img_res,
-                                     "bboxes": target['boxes'],
-                                     "labels": labels})
-            sample['image'] = torch.Tensor(sample['image'])
+            sample = self.transforms(image = img_res,
+                                    bboxes = target['boxes'],
+                                    labels = labels)
             img_res = sample['image']
             target['boxes'] = torch.Tensor(sample['bboxes'])
             
@@ -154,23 +154,18 @@ def plot_img_bbox(img, target):
 # Send train=True for training transforms and False for val/test transforms
 def get_transform(train):
   if train:
-    return v2.Compose(
+    return A.Compose(
         [
-            v2.RandomHorizontalFlip(0.5),
+            A.HorizontalFlip(0.5),
             # ToTensorV2 converts image to pytorch tensor without div by 255
-            # ToTensorV2(p=1.0), # Depricated
-            # v2.ToImage(),
-            v2.ToDtype(torch.float32, scale=True)
+            ToTensorV2(p=1.0) 
         ],
-        # bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']}
+        bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']}
     )
   else:
-    return v2.Compose(
-        [
-            # ToTensorV2(p=1.0), # Depricated
-            v2.ToDtype(torch.float32, scale=True)
-        ],
-        # bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']}
+    return A.Compose(
+        [ToTensorV2(p=1.0)],
+        bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']}
     )
 
 
@@ -245,7 +240,7 @@ optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
 checkpoint_file = "./checkpoints/model1.pth"
 
 if os.path.exists(checkpoint_file):
-    checkpoint = torch.load(checkpoint_file, map_location=torch.device(device))
+    checkpoint = torch.load(checkpoint_file, map_location=device)
     model.load_state_dict(checkpoint['model_state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     last_epoch = checkpoint['epoch']
@@ -263,11 +258,8 @@ lr_scheduler = torch.optim.lr_scheduler.StepLR(
 )
 
 
-# training for some number of epochs
-num_epochs = 35
-
-if num_epochs <= last_epoch + 1:
-    print(f"Number of epochs already exceeded. Current number of epochs is {last_epoch + 1}.")
+# training for 5 epochs
+num_epochs = 20
 
 for epoch in range(last_epoch + 1, num_epochs):
     # training for one epoch
@@ -287,3 +279,4 @@ for epoch in range(last_epoch + 1, num_epochs):
 
     print("Woo! Finished an epoch!")
 
+    
