4951ba2d99a03f880d382abe09920a35bdd2923d
dapeef <42572235+dapeef@users.noreply.github.com>
2023-12-30 11:19:22 +0000

    Modularise training

--- nn_utils.py
+++ nn_utils.py
@@ -1,29 +1,58 @@
+# basic python and ML Libraries
+import os
+# import random
+import numpy as np
+# import pandas as pd
 
+# for ignoring warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+# We will be reading images using OpenCV
+import cv2
+
+# matplotlib for visualization
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+# torchvision libraries
 import torch
 import torchvision
-from torchvision.transforms import v2
+# from torchvision.transforms import v2
+# from torchvision.transforms import ToTensor
+# from torchvision import transforms as torchtrans  
+# from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
-# Send train=True for training transforms and False for val/test transforms
-def get_transform(train):
-  if train:
-    return v2.Compose(
-        [
-            v2.RandomHorizontalFlip(0.5),
-            # ToTensorV2 converts image to pytorch tensor without div by 255
-            # ToTensorV2(p=1.0), # Depricated
-            # v2.ToImage(),
-            v2.ToDtype(torch.float32, scale=True)
-        ],
-        # bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']}
-    )
-  else:
-    return v2.Compose(
-        [
-            # ToTensorV2(p=1.0), # Depricated
-            v2.ToDtype(torch.float32, scale=True)
-        ],
-        # bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']}
-    )
+# # helper libraries
+import engine
+import utils
+import transforms as T
+
+# for image augmentations
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+
+
+# Function to visualize bounding boxes in the image
+def plot_img_bbox(img, target):
+    # plot the image and bboxes
+    # Bounding boxes are defined as follows: x-min y-min width height
+    fig, a = plt.subplots(1,1)
+    fig.set_size_inches(5,5)
+    a.imshow(img)
+    for i, box in enumerate(target['boxes']):
+        x, y, width, height = box[0], box[1], box[2]-box[0], box[3]-box[1]
+        rect = patches.Rectangle(
+        (x, y),
+        width, height,
+        linewidth = 2,
+        edgecolor = ['red', 'orange', 'yellow'][target["labels"][i]],
+        facecolor = 'none'
+        )
+        # Draw the bounding box on top of the image
+        a.add_patch(rect)
+    plt.show()
+
 
 
 # Get pretrained model
@@ -37,3 +66,79 @@ def get_object_detection_model(num_classes):
         torchvision.models.detection.faster_rcnn.FastRCNNPredictor(in_features, num_classes)
     
     return model
+
+def train_nn(dataset, dataset_test, checkpoint_file, num_epochs):
+    # define training and validation data loaders
+    data_loader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=10,
+        shuffle=True,
+        # num_workers=4,
+        collate_fn=utils.collate_fn,
+    )
+
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test,
+        batch_size=10,
+        shuffle=False,
+        # num_workers=4,
+        collate_fn=utils.collate_fn,
+    )
+
+
+    # train on gpu if available
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    print("Training on device:", device)
+
+    num_classes = 3 # one class (class 0) is dedicated to the "background"
+
+    # get the model using our helper function
+    model = get_object_detection_model(num_classes)
+
+    # move model to the right device
+    model.to(device)
+
+    # construct an optimizer
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
+
+    if os.path.exists(checkpoint_file):
+        checkpoint = torch.load(checkpoint_file, map_location=device)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        last_epoch = checkpoint['epoch']
+        # loss = checkpoint['loss']
+
+    else:
+        last_epoch = -1
+
+    # and a learning rate scheduler which decreases the learning rate by
+    # 10x every 3 epochs
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(
+        optimizer,
+        step_size=3,
+        gamma=0.1
+    )
+
+
+    if num_epochs <= last_epoch + 1:
+        print(f"Requested number of epochs ({num_epochs}) already exceeded; {last_epoch + 1} epochs completed")
+
+
+    for epoch in range(last_epoch + 1, num_epochs):
+        # training for one epoch
+        engine.train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+        # update the learning rate
+        lr_scheduler.step()
+
+        torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                # 'loss': loss
+                }, checkpoint_file)
+
+        # evaluate on the test dataset
+        engine.evaluate(model, data_loader_test, device=device)
+
+        print("Woo! Finished an epoch!")
--- pytorch_detection_test.py
+++ pytorch_detection_test.py
@@ -32,11 +32,11 @@ import transforms as T
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
 
+import nn_utils
 
 
 # we create a Dataset class which has a __getitem__ function and a __len__ function
 class TableImagesDataset(torch.utils.data.Dataset):
-
     def __init__(self, files_dir, width, height, transforms=None):
         self.transforms = transforms
         self.images_dir = files_dir + "/images"
@@ -130,56 +130,19 @@ class TableImagesDataset(torch.utils.data.Dataset):
         return len(self.imgs)
 
 
-# Function to visualize bounding boxes in the image
-def plot_img_bbox(img, target):
-    # plot the image and bboxes
-    # Bounding boxes are defined as follows: x-min y-min width height
-    fig, a = plt.subplots(1,1)
-    fig.set_size_inches(5,5)
-    a.imshow(img)
-    for i, box in enumerate(target['boxes']):
-        x, y, width, height = box[0], box[1], box[2]-box[0], box[3]-box[1]
-        rect = patches.Rectangle(
-        (x, y),
-        width, height,
-        linewidth = 2,
-        edgecolor = ['red', 'orange', 'yellow'][target["labels"][i]],
-        facecolor = 'none'
-        )
-        # Draw the bounding box on top of the image
-        a.add_patch(rect)
-    plt.show()
+transform = A.Compose(
+    [
+        A.HorizontalFlip(0.5),
+        # ToTensorV2 converts image to pytorch tensor without div by 255
+        ToTensorV2(p=1.0) 
+    ],
+    bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']}
+)
 
-
-# Send train=True for training transforms and False for val/test transforms
-def get_transform(train):
-  if train:
-    return A.Compose(
-        [
-            A.HorizontalFlip(0.5),
-            # ToTensorV2 converts image to pytorch tensor without div by 255
-            ToTensorV2(p=1.0) 
-        ],
-        bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']}
-    )
-  else:
-    return A.Compose(
-        [ToTensorV2(p=1.0)],
-        bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']}
-    )
-
-
-# Get pretrained model
-def get_object_detection_model(num_classes):
-    # load a model pre-trained pre-trained on COCO
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-    # get number of input features for the classifier
-    in_features = model.roi_heads.box_predictor.cls_score.in_features
-    # replace the pre-trained head with a new one
-    model.roi_heads.box_predictor = \
-        torchvision.models.detection.faster_rcnn.FastRCNNPredictor(in_features, num_classes)
-    
-    return model
+# eval_transform = A.Compose(
+#     [ToTensorV2(p=1.0)],
+#     bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']}
+# )
 
 
 # defining the files directory and testing directory
@@ -187,8 +150,8 @@ files_dir = 'data/Pockets, cushions, table - 2688 - B&W, rotated, mostly 9 ball/
 test_dir = 'data/Pockets, cushions, table - 2688 - B&W, rotated, mostly 9 ball/test'
 
 # construct dataset
-dataset = TableImagesDataset(files_dir, 244, 244, transforms=get_transform(True))
-dataset_test = TableImagesDataset(test_dir, 244, 244, transforms=get_transform(True))
+dataset = TableImagesDataset(files_dir, 244, 244, transforms=transform)
+dataset_test = TableImagesDataset(test_dir, 244, 244, transforms=transform)
 print('Length of training dataset:', len(dataset))
 print('Length of test dataset:', len(dataset_test))
 
@@ -202,81 +165,12 @@ print('Length of test dataset:', len(dataset_test))
 # plot_img_bbox(img, target)
 
 
-# define training and validation data loaders
-data_loader = torch.utils.data.DataLoader(
-    dataset,
-    batch_size=10,
-    shuffle=True,
-  	# num_workers=4,
-    collate_fn=utils.collate_fn,
-)
-
-data_loader_test = torch.utils.data.DataLoader(
-    dataset_test,
-    batch_size=10,
-    shuffle=False,
-    # num_workers=4,
-    collate_fn=utils.collate_fn,
-)
-
-
-# train on gpu if available
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-print("Device:", device)
-
-num_classes = 3 # one class (class 0) is dedicated to the "background"
-
-# get the model using our helper function
-model = get_object_detection_model(num_classes)
-
-# move model to the right device
-model.to(device)
-
-# construct an optimizer
-params = [p for p in model.parameters() if p.requires_grad]
-optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
 
 # Load from last checkpoint
-checkpoint_file = "./checkpoints/model1.pth"
-
-if os.path.exists(checkpoint_file):
-    checkpoint = torch.load(checkpoint_file, map_location=device)
-    model.load_state_dict(checkpoint['model_state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    last_epoch = checkpoint['epoch']
-    # loss = checkpoint['loss']
-
-else:
-    last_epoch = -1
-
-# and a learning rate scheduler which decreases the learning rate by
-# 10x every 3 epochs
-lr_scheduler = torch.optim.lr_scheduler.StepLR(
-    optimizer,
-    step_size=3,
-    gamma=0.1
-)
-
+checkpoint_file = "./checkpoints/model.pth"
 
 # training for 5 epochs
-num_epochs = 20
+num_epochs = 40
 
-for epoch in range(last_epoch + 1, num_epochs):
-    # training for one epoch
-    train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
-    # update the learning rate
-    lr_scheduler.step()
 
-    torch.save({
-            'epoch': epoch,
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            # 'loss': loss
-            }, checkpoint_file)
-
-    # evaluate on the test dataset
-    evaluate(model, data_loader_test, device=device)
-
-    print("Woo! Finished an epoch!")
-
-    
+nn_utils.train_nn(dataset, dataset_test, checkpoint_file, num_epochs)
