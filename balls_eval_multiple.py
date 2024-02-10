aa03a0ea8d09df3f553b4f97ba3b2750a44d33af
dapeef <42572235+dapeef@users.noreply.github.com>
2024-02-10 18:54:57 +0000

    Add profiler

--- balls_eval_multiple.py
+++ balls_eval_multiple.py
@@ -5,12 +5,21 @@ import cv2
 # for ignoring warnings
 import warnings
 warnings.filterwarnings('ignore')
+import time
+import cProfile
 
 
 def evaluate(image_file):
+    start_time = time.time()
     evaluator = nn_utils.EvaluateNet("./checkpoints/balls_model_multiple.pth", 20)
+    print(f"Model load time: {time.time() - start_time}")
+    start_time = time.time()
     evaluator.create_dataset(image_file)
+    print(f"Dataset creation time: {time.time() - start_time}")
+    start_time = time.time()
     target = evaluator.get_boxes(0)
+    print(f"Model eval time: {time.time() - start_time}")
+    start_time = time.time()
 
     return target
 
@@ -33,15 +42,22 @@ def get_balls(image_file, confidence_threshold=0.5):
     return target["centres"]
 
 
-if __name__ == "__main__":
+def _test():
     image_file = "./images/terrace2.jpg"
 
     target = evaluate(image_file)
 
     for i in range(0, 10):
-        print(f"Getting with confidence of {i/10}...")
+        print(f"Getting with confidence >= {i/10}...")
         filter_show(target, image_file, confidence_threshold=i/10)
     
     print(target["scores"])
 
     plt.show()
+
+    
+if __name__ == "__main__":
+    # Profiler
+    # cProfile.run('for i in range(10): _test()', filename="./temp/balls_eval.prof")
+
+    _test()
